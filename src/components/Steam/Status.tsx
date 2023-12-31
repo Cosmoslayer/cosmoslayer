a58d2aaab52c9a9d2b60d7e5b86e10b150ac4e86
Mark Stephen Gonzales <cosmoslayer@gmail.com>
2024-01-01 05:41:51 +0800

    Fix double quote escape character tag error.

--- src/components/Steam/Status.tsx
+++ src/components/Steam/Status.tsx
@@ -12,24 +12,24 @@ export default function PersonaState({
   lastLogOff: number
 }) {
   if (gameextrainfo) {
-    return <Typography variant='h6'>&quotI am currently playing {gameextrainfo}</Typography>
+    return <Typography variant='h6'>&quot;I am currently playing {gameextrainfo}&ldquo;</Typography>
   }
   else {
     switch (personaState) {
       case 0:
-        return <Typography variant='h6'>&quotI was last seen {moment.unix(lastLogOff).fromNow()}&ldquo</Typography>
+        return <Typography variant='h6'>&quot;I was last seen {moment.unix(lastLogOff).fromNow()}&ldquo;</Typography>
       case 1:
-        return <Typography variant='h6'>&quotI am currently Online&ldquo</Typography>
+        return <Typography variant='h6'>&quot;I am currently Online&ldquo;</Typography>
       case 2:
-        return <Typography variant='h6'>&quotI am currently Busy&ldquo</Typography>
+        return <Typography variant='h6'>&quot;I am currently Busy&ldquo;</Typography>
       case 3:
-        return <Typography variant='h6'>&quotI am currently Away&ldquo</Typography>
+        return <Typography variant='h6'>&quot;I am currently Away&ldquo;</Typography>
       case 4:
-        return <Typography variant='h6'>&quotI am currently Snoozing&ldquo</Typography>
+        return <Typography variant='h6'>&quot;I am currently Snoozing&ldquo;</Typography>
       case 5:
-        return <Typography variant='h6'>&quotI am currently looking to trade&ldquo</Typography>
+        return <Typography variant='h6'>&quot;I am currently looking to trade&ldquo;</Typography>
       case 6:
-        return <Typography variant='h6'>&quotI am currently looking to play&ldquo</Typography>
+        return <Typography variant='h6'>&quot;I am currently looking to play&ldquo;</Typography>
     }
   }
   return null;
